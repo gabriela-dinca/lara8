1e56829855db2ccee13cb773bd1be31e6ecd5a8f
Gabi <gabriela.dinca@codenground.de>
2022-02-06 16:32:20 +0100

    added blade layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,9 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-    <link rel="stylesheet" href="/app.css"></link>
-</head>
-<body>
+@extends('layout')
+@section('content')
     @foreach ($posts as $post)
         <article class="{{ $loop->first ? 'mt-4' : '' }}">
             <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
-</body>
-</html>
+@endsection
