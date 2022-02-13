676f82a1a03c3fd03cb0cc33f0564333b59734d0
Gabi <gabriela.dinca@codenground.de>
2022-02-13 15:43:47 +0100

    added eager relationships and clockwork dev requirement

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,11 @@
 
 use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static function () {
-    DB::listen(static fn ($query) => logger($query->sql, $query->bindings));
-    return view('posts', ['posts' => Post::all()]);
-});
-
-//Route::get('/', static fn () => view('posts', ['posts' => Post::all()]) );
+Route::get('/', static fn () =>
+    view('posts', ['posts' => Post::with('category')->get()])
+);
 
 Route::get('/posts/{post:slug}', static fn (Post $post)  =>
      view('post', [
