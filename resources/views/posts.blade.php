33939c41218f2a21b6eb88959fedda3911402383
Gabi <gabriela.dinca@codenground.de>
2022-03-07 10:58:13 +0100

    added design to blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,11 @@
 @extends('layout')
 @section('content')
-    @foreach ($posts as $post)
-        <article class="{{ $loop->first ? 'mt-4' : '' }}">
-            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">
-                    {{ $post->author->first_name }} {{ $post->author->last_name }}
-                </a> in
-                <a href="/categories/{{ $post->category->slug }}">
-                    {{ $post->category->name }}
-                </a>
-            </p>
-            <p>{!! $post->excerpt !!}</p>
-        </article>
-    @endforeach
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->isNotEmpty())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
+    </main>
 @endsection
