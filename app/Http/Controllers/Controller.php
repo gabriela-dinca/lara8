f46eba61a2f3c973df59c80af447996718b9a7d5
Gabi <gabriela.dinca@codenground.de>
2022-03-08 12:02:37 +0100

    outsourced search to the controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +15,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(): Factory|View|Application
+    {
+        return view('posts', [
+            'posts' => Post::latest('published_at')->filter(request(['search']))->with(['category', 'author'])->get(),
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Post $post): Factory|View|Application
+    {
+        return view('post', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Post extends Model
 {
@@ -13,6 +15,15 @@ class Post extends Model
     //protected $guarded = [];
     protected $fillable = ['title', 'excerpt', 'body', 'category_id'];
     //protected $with = ['category', 'author'];
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query
+                ->where('title', 'like', '%'.$search.'%')
+                ->orWhere('body', 'like', '%'.$search.'%')
+        );
+    }
 
     public function category(): BelongsTo {
         return $this->belongsTo(Category::class);
