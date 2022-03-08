dde4929dc3a333b4095130a9fac319da18f833b4
Gabi <gabriela.dinca@codenground.de>
2022-03-08 09:22:58 +0100

    added categories dropdown

--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,31 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', static fn () =>
-    view('posts', ['posts' => Post::latest('published_at')->with(['category', 'author'])->get()])
+    view('posts', [
+        'posts' => Post::latest('published_at')->with(['category', 'author'])->get(),
+        'categories' => Category::all()
+    ])
 );
 
 Route::get('/posts/{post:slug}', static fn (Post $post)  =>
      view('post', [
-        'post' => $post
+        'post' => $post,
+         'categories' => Category::all()
     ])
 );
 
 Route::get('/categories/{category:slug}', static fn (Category $category)  =>
      view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ])
 );
 
 Route::get('/authors/{author:username}', static function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
 
