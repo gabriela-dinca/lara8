fd61ae577b6786f08023c18eb8d0167bc8b1f45a
Gabi <gabriela.dinca@codenground.de>
2022-03-14 23:43:57 +0100

    merge the query filters

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,9 +19,11 @@ class Post extends Model
     public function scopeFilter(Builder $query, array $filters): void
     {
         $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query
-                ->where('title', 'like', '%'.$search.'%')
-                ->orWhere('body', 'like', '%'.$search.'%')
+            $query->where(fn($query) =>
+                $query
+                    ->where('title', 'like', '%'.$search.'%')
+                    ->orWhere('body', 'like', '%'.$search.'%')
+            )
         );
 
         $query->when($filters['category'] ?? false, fn($query, $categorySlug) =>
