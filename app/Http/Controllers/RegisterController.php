7939bc14474aa05844faef96389b3c08c835e5dd
Gabi <gabriela.dinca@codenground.de>
2022-04-19 01:28:29 +0200

    added login and logout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -26,7 +26,9 @@ class RegisterController extends Controller
             'password' => 'required|min:7|max:255'
         ]);
 
-        User::create($attributes);
+        $newUser = User::create($attributes);
+
+        auth()->login($newUser);
 
         return redirect('/')->with('success', 'Your account has been created.');
     }
---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SessionController extends Controller
+{
+    public function destroy(){
+       auth()->logout();
+       return redirect('/')->with('success', 'Goodbye!');
+    }
+}
