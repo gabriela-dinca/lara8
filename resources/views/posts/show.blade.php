c9b50f736065dc553efd85d075e357da8a853e83
Gabi <gabriela.dinca@codenground.de>
2022-05-08 18:37:08 +0200

    added comment component

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,6 @@
     <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
         <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
             <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                {{--todo--}}
                 <img src="/images/illustration-1.png" alt="" class="rounded-xl">
 
                 <p class="mt-4 block text-gray-400 text-xs">
@@ -45,6 +44,10 @@
                     {!! $post->body  !!}
                 </div>
             </div>
+
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-post-comment></x-post-comment>
+            </section>
         </article>
     </main>
 @endsection
