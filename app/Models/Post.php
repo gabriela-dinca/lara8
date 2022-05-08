c8ccc45f986adc0e96b4b33ca58fc6d7b57c7691
Gabi <gabriela.dinca@codenground.de>
2022-05-08 18:58:17 +0200

    added comment migration, model and factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 class Post extends Model
@@ -45,5 +46,10 @@ class Post extends Model
 
     public function author(): BelongsTo{
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'user_id');
     }
 }
